e88c3265c83a1ace09d1a467fb2d30bbb80ba591
lazarofonseca <lazaronobrega3@gmail.com>
2022-03-17 20:06:16 -0300

    Iniciando Projeto

---
+++ api-partidos/src/main/java/br/com/lazaro/api/ApiPartidosApplication.java
@@ -0,0 +1,13 @@
+package br.com.lazaro.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPartidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPartidosApplication.class, args);
+	}
+
+}
---
+++ api-partidos/src/test/java/br/com/lazaro/api/ApiPartidosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lazaro.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPartidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
