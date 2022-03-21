8e2c9f1349b2f5893d86d545fe298564f659f079
lazarofonseca <lazaronobrega3@gmail.com>
2022-03-21 11:22:35 -0300

    Testes do Partido service

--- api-partidos/src/test/java/br/com/lazaro/api/service/ApiServicePartidoTests.java
+++ api-partidos/src/test/java/br/com/lazaro/api/service/ApiServicePartidoTests.java
@@ -1,5 +1,12 @@
 package br.com.lazaro.api.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.doNothing;
+
+import java.time.LocalDate;
+import java.util.List;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,53 +19,64 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import br.com.lazaro.api.dto.AssociadoDTO;
+import br.com.lazaro.api.dto.PartidoDTO;
 import br.com.lazaro.api.model.Associado;
-import br.com.lazaro.api.repository.AssociadoRepository;
-import br.com.lazaro.api.service.exception.EntityNotFoundServiceException;
+import br.com.lazaro.api.model.Partido;
+import br.com.lazaro.api.repository.PartidoRepository;
 
 @ExtendWith(SpringExtension.class)
-public class ApiServiceAssociadoTests {
+public class ApiServicePartidoTests {
 
 	@InjectMocks
-	private AssociadoService service;
-	
-	private AssociadoDTO associadoDTO;
-	
-	private Associado associado;
-	
-	
+	private PartidoService service;
 
 	@Mock
-	private AssociadoRepository repository;
+	private PartidoDTO partidodoDTO;
+	
+	@Mock
+	private Partido partido;
+
+	@Mock
+	private PartidoRepository repository;
 
 	private Long idExistente;
 	private Long idNaoExiste;
 	private Long idErroIntegridade;
-	private String nomeAssociado;
-	private String cargoPolitico;
-	private String sexo;
+
 
 	@BeforeEach
 	void inicializaId() throws Exception {
-		
-		
-		nomeAssociado = "Lázaro";
-		cargoPolitico = "Vereador";
-		sexo = "Vereador";
+
+		instanciaPartidoDTO();
+
 		idExistente = 1L;
 		idNaoExiste = 10000L;
 		idErroIntegridade = 50L;
-		
 
 		Mockito.doNothing().when(repository).deleteById(idExistente);
 		Mockito.doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(idNaoExiste);
 		Mockito.doThrow(DataIntegrityViolationException.class).when(repository).deleteById(idErroIntegridade);
 	}
-	
-	
 
 	@Test
-	public void deveriaDeletarQuandoIdExistirSemAssociacaoAoPartido() {
+	public void deveriaBuscarTodosPartidos() {
+
+		Mockito.when(repository.findAll()).thenReturn(List.of(partido));
+
+		List<PartidoDTO> response = service.findAll();
+
+		assertNotNull(response);
+		assertEquals(1, response.size());
+		assertEquals(PartidoDTO.class, response.get(0).getClass());
+		assertEquals(1L, response.get(0).getIdPartido());
+		assertEquals("Partido Neutro", response.get(0).getNomePartido());
+		assertEquals("PN", response.get(0).getSigla());
+		assertEquals("Centro", response.get(0).getIdeologia());
+
+	}
+
+	@Test
+	public void deveriaDeletarQuandoIdPartidoExistir() {
 
 		Assertions.assertDoesNotThrow(() -> {
 			service.delete(idExistente);
@@ -71,7 +89,7 @@ public class ApiServiceAssociadoTests {
 	@Test
 	public void deveriaLancarExcecaoParaIdNaoExistente() {
 
-		Assertions.assertThrows(EntityNotFoundServiceException.class, () -> {
+		Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
 			service.delete(idNaoExiste);
 		});
 
@@ -82,11 +100,25 @@ public class ApiServiceAssociadoTests {
 	@Test
 	public void deveriaLancarExcecaoParaErroDeIntegridadeDeDados() {
 
-		Assertions.assertThrows(EntityNotFoundServiceException.class, () -> {
+		Assertions.assertThrows(DataIntegrityViolationException.class, () -> {
 			service.delete(idErroIntegridade);
 		});
 
 		Mockito.verify(repository, Mockito.times(1)).deleteById(idErroIntegridade);
+
+	}
+	
+	
+
+	private void instanciaPartidoDTO() {
+
+		PartidoDTO partido = new PartidoDTO(1L, "Partido Neutro", "PN", "Centro", LocalDate.now());
+
+	}
+
+	private void instanciaPartido() {
+
+		PartidoDTO partido = new PartidoDTO(1L, "Partido Neutro", "PN", "Centro", LocalDate.now());
 
 	}
 
